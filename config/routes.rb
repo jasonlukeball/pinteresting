daf2072d9279b312dfe4b3e8d3c7f42bdd2445f8
Jason Ball <jasonlukeball@me.com>
2015-04-13 15:49:56 +1000

    Changed the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+
+  # this routes http://localhost:3000 to app/views/pages/home.html.erb
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
