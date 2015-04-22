73723d12aa082a8a94d46ee88d433ef93f717865
Jason Ball <jasonlukeball@me.com>
2015-04-22 16:50:13 +1000

    Revert 2f98c7f..358a5d0
    This rolls back to commit 2f98c7fca545ac4eb7e214f71ffec18092d3db23.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,11 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-
-  # Required for Heroku
-  config.action_mailer.default_url_options = { :host => 'superpinteresting.herokuapp.com' }
-
-
-
 end
