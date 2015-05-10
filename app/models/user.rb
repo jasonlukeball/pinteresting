1b88c7a42f5c6568fd3184bf108dca77ce3116d3
Jason Ball <jasonlukeball@me.com>
2015-05-11 09:25:22 +1000

    Added validation to users and pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # RELATIONSHIP TO PINS - ONE USE HAS MANY PINS
   has_many :pins
 
   # VALIDATION - MUST GIVE USERNAME
