00e6322bca5d7fb4119312af24b6a53e2675f643
Jason Ball <jasonlukeball@me.com>
2015-05-10 21:54:48 +1000

    Added username field to users.rb , updated application controller and new/edit views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # THIS IS FOR THE CREATE ACCOUNT AND UPDATE USER ACCOUNT METHODS FOR DEVISE
+  # IT ALLOWS THE NEW FIELD (NAME) TO BE INCLUDED IN THE CREATION OR UPDATING ON A USER RECORD
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
