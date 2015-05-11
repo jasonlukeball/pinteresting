3ef936232f988f41aedb4a31c0ef82d71fed366c
Jason Ball <jasonlukeball@me.com>
2015-05-11 10:59:39 +1000

    more app.rb fun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # THIS IS FOR THE CREATE ACCOUNT AND UPDATE USER ACCOUNT METHODS FOR DEVISE
-  # IT ALLOWS THE NEW FIELD (NAME) TO BE INCLUDED IN THE CREATION OR UPDATING ON A USER RECORD
 
-
+end
