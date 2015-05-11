983556bf7d70a3e5f9284ec919092ff636c054bd
Jason Ball <jasonlukeball@me.com>
2015-05-11 10:57:02 +1000

    removed stuff from app.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,4 @@ class ApplicationController < ActionController::Base
   # THIS IS FOR THE CREATE ACCOUNT AND UPDATE USER ACCOUNT METHODS FOR DEVISE
   # IT ALLOWS THE NEW FIELD (NAME) TO BE INCLUDED IN THE CREATION OR UPDATING ON A USER RECORD
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
-  end
-end
